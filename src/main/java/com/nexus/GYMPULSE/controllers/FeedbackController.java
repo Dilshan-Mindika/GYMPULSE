7b19499b3cdd56a1d0162048c906364669e54595
Dilshan Mindika <maxdilshan666@gmail.com>
2024-11-25 02:45:00 +0530

    updated

--- src/main/java/com/nexus/GYMPULSE/controllers/FeedbackController.java
+++ src/main/java/com/nexus/GYMPULSE/controllers/FeedbackController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173/Feedbacks")
 @RestController
 @RequestMapping("/feedbacks")
 public class FeedbackController {
--- src/main/java/com/nexus/GYMPULSE/controllers/TrainerController.java
+++ src/main/java/com/nexus/GYMPULSE/controllers/TrainerController.java
@@ -6,19 +6,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.nexus.GYMPULSE.model.person.Trainer;
 import com.nexus.GYMPULSE.requests.TrainerRequest;
 import com.nexus.GYMPULSE.service.interfaces.TrainerService;
 
+@CrossOrigin(origins = "http://localhost:5173/Trainers")
 @RestController
 @RequestMapping("/Trainers") // Base URL for trainer-related endpoints
 public class TrainerController {
